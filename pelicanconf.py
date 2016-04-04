bca81fd60e476c5fb9ca12b1888db48bb1becdde
cvrdwthnts <hegge.mark@gmail.com>
2016-04-03 23:06:58 -0700

    added theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Canada/Pacific'
 
 DEFAULT_LANG = 'en'
+THEME = "/Users/mark/pelican-themes/mnmlist"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
